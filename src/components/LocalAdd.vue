82729080c7c03e38745bda4ebdedd499d9805e9d
Mac-Lee0502 <qq1157725063@outlook.com>
2020-01-07 16:58:11 +0800

    Local Add

---
+++ src/components/LocalAdd.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col
+          cols="2"
+          md="1"
+        >  
+          <v-text-field
+          v-model="aa"
+          label="aa"
+          ></v-text-field>
+        
+        </v-col>
+
+        <v-btn>+</v-btn>
+
+        <v-col
+          cols="2"
+          md="1"
+        >
+          <v-text-field
+          v-model="bb"
+          label="bb"
+          ></v-text-field>
+        </v-col>
+        
+        <v-btn>=</v-btn>
+
+        <v-col
+          cols="2"
+          md="1"
+        >  
+          <v-text-field
+          v-model="ss"
+          label="???"
+          >
+        </v-text-field>
+        </v-col>
+        <v-btn @click="add">add</v-btn>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+
+  export default {
+    data: () => ({
+      valid: false,
+      aa: "",
+      bb: "",
+      ss: ""
+    }),
+    methods: {
+        add () {
+            this.ss = Number(this.aa) + Number(this.bb);
+        }
+    }
+  }
+</script>
