f6b3471e5a5e5c16a41293cd9b28177edad7805c
Mac-Lee0502 <qq1157725063@outlook.com>
2020-01-08 20:22:46 +0800

    Local Add
    Resolve #1
    
    Signed-off-by: Mac-Lee0502 <qq1157725063@outlook.com>

--- src/components/LocalAdd.vue
+++ src/components/LocalAdd.vue
@@ -1,44 +1,53 @@
 <template>
-  <v-form v-model="valid">
+  <v-form>
     <v-container>
       <v-row>
         <v-col
           cols="2"
           md="1"
         >  
-          <v-text-field
-          v-model="aa"
-          label="aa"
-          ></v-text-field>
-        
+        <v-text-field
+        v-model="aa"
+        label="aa"
+        ></v-text-field>
         </v-col>
 
+        <v-col
+        cols="2"
+        md="1"
+        > 
         <v-btn>+</v-btn>
+        </v-col>
 
         <v-col
           cols="2"
           md="1"
         >
-          <v-text-field
-          v-model="bb"
-          label="bb"
-          ></v-text-field>
+        <v-text-field
+        v-model="bb"
+        label="bb"
+        ></v-text-field>
         </v-col>
         
+        <v-col
+        cols="2"
+        md="1"
+        > 
         <v-btn>=</v-btn>
+        </v-col>
 
         <v-col
           cols="2"
           md="1"
         >  
-          <v-text-field
-          v-model="ss"
-          label="???"
-          >
+        <v-text-field
+        v-model="ss"
+        >
         </v-text-field>
         </v-col>
-        <v-btn @click="add">add</v-btn>
+
       </v-row>
+        <v-btn @click="add">add</v-btn>
     </v-container>
   </v-form>
 </template>
@@ -47,7 +56,6 @@
 
   export default {
     data: () => ({
-      valid: false,
       aa: "",
       bb: "",
       ss: ""
